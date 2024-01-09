8e8b0cefee0adaba33aa8d6dbdfc3ac854a6c395
Brian <bmaitner@gmail.com>
2024-01-09 15:22:56 -0500

    globals file

--- R/globals.R
+++ R/globals.R
@@ -2,3 +2,4 @@ utils::globalVariables(c("gid_0","gid_1","gid_2",
                          "countryBIEN","state_province","county",
                          "centroid_distance", "dist_max","cent_ID",
                          "country","county_parish"))
+
